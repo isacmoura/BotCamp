decf50a8f2af871a48ae2fcdfd05f9f5fd012d9f
Isac Moura <contatoisacmoura@outlook.com>
2019-04-05 23:40:30 -0300

    Desafio 1

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import BtnBotcamp from './components/BtnBotcamp/';
+import LogoBotcamp from './components/Logo';
 
 class App extends Component{
     render() {
-        return <BtnBotcamp />
+        return <div>
+            <LogoBotcamp />
+            <BtnBotcamp />
+        </div>
     }
 
 }
---
+++ app/src/components/Logo/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Logo from './img/botcamp.png'
+
+const LogoBotcamp = () => (
+    <img src={Logo} className='logo-botcamp' />
+);
+
+export default LogoBotcamp
