526fb5e76c925a3a9d8823a581aee3e909849669
Isac Moura <contatoisacmoura@outlook.com>
2019-04-05 23:51:14 -0300

    Desafio 2

--- app/src/components/Logo/index.js
+++ app/src/components/Logo/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Logo from './img/botcamp.png'
+import './style.css';
 
 const LogoBotcamp = () => (
-    <img src={Logo} className='logo-botcamp' />
+    <img src={Logo} className='logo-botcamp' alt="Logo BotCamp" />
 );
 
 export default LogoBotcamp
